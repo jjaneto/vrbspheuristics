26173be1285b387ef9777b77a096d72eed5f7a20
José Joaquim <jjandradent@gmail.com>
2020-05-10 03:04:58 -0300

    putting the right script

--- scriptVNS.sh
+++ scriptVNS.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-instancias=("U_8")
+instancias=("U_16")
 mod="D250x250"
 timeLimit=10
 
 for inst in "${instancias[@]}"; do
-  mkdir ${inst}
+  mkdir -p results/${inst}
+  pathResults="results/${inst}"
   objectivesFile="objectivesFile.txt"
   for ((i = 1; i <= 1; i++)); do
     file="${mod}/${inst}/${inst}_${i}.txt"
     solutionsFile="solutionFile_"${inst}_${i}".txt";
     echo "Trying experiment ${file}"
-    ./main ../Instancias/${file} solutionsFile objectivesFile timeLimit
+    ./main ../Instancias/${file} ${pathResults}/"${solutionsFile}" ${pathResults}/"${objectivesFile}" ${timeLimit}
   done
 done
